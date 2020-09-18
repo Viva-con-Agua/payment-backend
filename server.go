a5ae414386e7cabf61ee242a10180ad0571ba911
Dennis Kleber <dennis_kleber@mailbox.org>
2020-09-18 17:29:49 +0200

    change routes

--- server.go
+++ server.go
@@ -31,10 +31,10 @@ func main() {
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 	e.Validator = &CustomValidator{validator: validator.New()}
-	e.POST("/v1/payments/card", controller.PaymentIntentCard)
-	e.POST("/v1/payments/iban", controller.PaymentIntentIBAN)
-	e.POST("/v1/payments/success", controller.SuccessPayment)
-	e.POST("/v1/payments/default", controller.AddDefaultPayment)
-	e.POST("/v1/payments/subscription", controller.Subscription)
+	e.POST("/v1/payment/card", controller.PaymentIntentCard)
+	e.POST("/v1/payment/iban", controller.PaymentIntentIBAN)
+	e.POST("/v1/payment/success", controller.SuccessPayment)
+	e.POST("/v1/payment/default", controller.AddDefaultPayment)
+	e.POST("/v1/payment/subscription", controller.Subscription)
 	e.Logger.Fatal(e.Start(":1323"))
 }
